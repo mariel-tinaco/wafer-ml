9d72e87fa5d18b3b1ef240710c4a2c84379811ad
Alteza <Nathaniel.Alteza@analog.com>
2022-10-06 17:01:50 +0800

    Update2

---
+++ augment.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import os
+# import shutil
+from PIL import Image, ImageOps
+import torch
+from torchvision import transforms
+
+from copy import copy
+
+data_dir = "ADC_Dataset"
+
+new_dir = f"{data_dir}_Augmented"
+
+torch.manual_seed(0)
+aug = 0
+train_cnt = 0
+test_cnt = 0
+
+sd = (0.2, 5)
+
+def aug_flip(img, img_name, state, dst_dir, img_size = (128,128), train = False):
+    prefix = "f"
+
+    for i in range(4):
+        aug_img_name = "{}_{}{}".format(img_name, prefix, i)
+
+        if i > 1:
+            img = ImageOps.mirror(img)
+        if i % 2 == 0:
+            img = ImageOps.flip(img)
+
+        #rotate resulting images
+        aug_rotate(img, aug_img_name, state, dst_dir=dst_dir, img_size=img_size, train = train)
+
+def aug_rotate(img, img_name, state, dst_dir, img_size = (128,128), train = False):
+    prefix = "r"
+    
+    for i in range(4):
+        aug_img_name = "{}_{}{}".format(img_name, prefix, i)
+        img = img.rotate(i*90)
+
+        #blur, rotate and/or add jitter to resulting images
+        augment(img, aug_img_name, state, dst_dir=dst_dir, img_size=img_size, train = train)
+
+
+
+def augment(img, img_name, state, dst_dir, img_size = (128,128), train = False):
+    global train_cnt
+    global test_cnt
+    file_ext = 'jpg'
+    if train:    
+        img_dst = os.path.join(dst_dir, 'train', state, img_name)
+        train_cnt += 1
+    else:
+        img_dst = os.path.join(dst_dir, 'test', state, img_name)
+        test_cnt += 1
+
+    res_img = img.resize(img_size)
+    res_img.save("{}.{}".format(img_dst, file_ext))
+
+    if train and aug>0:
+        # crop center & blur only
+        aug_img_file = "{}_{}{}.{}".format(img_dst, 'b', 0, file_ext)
+        aug_img = augment_blur(img)
+        aug_img = aug_img.resize(img_size)
+        aug_img.save(aug_img_file)
+        train_cnt += 1
+
+        # random jitter, affine, brightness & blur
+        for i in range(aug):
+            aug_img_file = "{}_{}{}.{}".format(img_dst, 'j', i, file_ext)
+            aug_img = augment_affine_jitter_blur(img)
+            aug_img = aug_img.resize(img_size)
+            aug_img.save(aug_img_file)
+            train_cnt += 1
+
+
+def augment_affine_jitter_blur(orig_img):
+    """
+    Augment with multiple transformations
+    """
+    train_transform = transforms.Compose([
+        transforms.RandomAffine(degrees=20, translate=(0.05, 0.05)),
+        #transforms.RandomPerspective(distortion_scale=0.3, p=0.2),
+        transforms.CenterCrop((220, 220)),
+        transforms.ColorJitter(brightness=(0.5)),
+        transforms.GaussianBlur(kernel_size=(5, 5), sigma=sd),
+        ])
+    return train_transform(orig_img)
+
+
+def augment_blur(orig_img):
+    """
+    Augment with center crop and bluring
+    """
+    train_transform = transforms.Compose([
+        #transforms.CenterCrop((240, 240)), #220,220
+        transforms.GaussianBlur(kernel_size=(5, 5), sigma=sd) #sigma 5
+        ])
+    return train_transform(orig_img)
+
+
+def create_set(aug = 0, img_size = (128,128)):
+    src_path = os.path.join(data_dir, "train")
+    dst_path = os.path.join(new_dir, "train")
+    if not os.path.isdir(new_dir):
+        os.makedirs(new_dir, exist_ok=True)
+        os.makedirs(dst_path, exist_ok=True)
+
+    for state in os.listdir(src_path):
+        train_img_dir = os.path.join(src_path, state)
+        dst_img_dir = os.path.join(dst_path, state)
+        os.makedirs(dst_img_dir, exist_ok=True)
+
+        for file in os.listdir(train_img_dir):
+            img_src = os.path.join(train_img_dir,file)
+            img_name = file.rsplit('.', 1)[0]
+
+            img = Image.open(img_src)
+
+            aug_flip(img, img_name, state, dst_dir=new_dir, img_size=img_size, train = True)
+
+
+    print(f'Augmented dataset: {test_cnt} test, {train_cnt} train samples')
+
+
+
+create_set(img_size=(480,480))
---
+++ train.py
@@ -0,0 +1,141 @@
+# from adc_dataset import ADC_Dataset
+import torchvision
+import torch.utils.data
+from torch.utils.tensorboard import SummaryWriter
+import math
+from datetime import datetime
+from models import test_model
+from sampler import StratifiedBatchSampler
+import numpy as np
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+
+torch.manual_seed(0)
+date_time = datetime.today().strftime('%Y%m%d-%H%M%S')
+use_cuda = torch.cuda.is_available()
+
+if __name__ == "__main__":
+    # this defines where we'll perform our training.  By default, we use the cpu.
+    # if you have a GPU and cuda set up properly, you can instead set this to 'cuda'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    save_folder = 'logs'
+#     model_name = 'mobilenetv2'
+    model_name = 'resnet18'
+#     data_set = 'ADC_Dataset'
+    data_set = 'ADC_Dataset_Augmented' 
+    train_dir = data_set + '/train'
+
+    # build our training data and validation data sets
+    # dataset = ADC_Dataset(f"{data_set}/train", training=True)
+    train_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((224,224)),
+    ])
+    dataset = torchvision.datasets.ImageFolder(train_dir, transform=train_transforms)
+    targets = dataset.targets
+    n_images = len(dataset)
+    classes = dataset.classes
+    num_classes = len(dataset.classes)
+    batch_size = 32
+    num_epochs = 20
+    
+    lr = 0.0001
+    wd = 0
+
+
+    train_idx, valid_idx= train_test_split(np.arange(len(targets)), test_size=0.1, shuffle=True, stratify=targets)
+    train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
+    valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
+    train_dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, num_workers=4, sampler=train_sampler)
+    validation_dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, num_workers=4, sampler=valid_sampler)
+
+    n_train = len(train_sampler)
+    n_val = len(valid_sampler)
+
+    
+    print("Loaded ", n_images, " images")
+    print("Training split: ", n_train)
+    print("Validation split: ", n_val)
+
+    #model = torchvision.models.mobilenet.mobilenet_v2(num_classes=num_classes)
+    model = test_model.ResNet34(num_classes=num_classes)
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    writer = SummaryWriter("./{}/{}-{}".format(save_folder, model_name, date_time))
+    steps_per_epoch = math.ceil(n_train/batch_size)
+
+    for epoch in range(num_epochs):
+        running_loss = 0
+        correct_preds = 0
+    
+        trainiter = iter(train_dataloader)
+
+        model.train()
+
+        for batch_images, batch_labels in trainiter:
+            optimizer.zero_grad()
+            batch_images = batch_images.to(device)
+            batch_labels = batch_labels.to(device)
+            outputs = model(batch_images)
+
+            batch_loss = loss_fn(outputs, batch_labels)
+            predictions = torch.argmax(outputs, dim=1)
+            correct_preds += torch.sum((predictions == batch_labels).float())
+
+            batch_loss.backward()
+            optimizer.step()
+
+            running_loss += outputs.shape[0]*batch_loss.item()
+
+        accuracy = correct_preds*100/n_train
+        epoch_loss = running_loss/n_train
+
+        writer.add_scalar("Training Loss", epoch_loss, epoch)
+        writer.add_scalar("Training Accuracy", accuracy, epoch)
+
+        print("Epoch: {} Loss: {:.4f} Accuracy: {:.2f}%".format
+            (epoch,
+            epoch_loss,
+            accuracy))
+
+
+#         print("RUNNING VAL")
+        validationiter = iter(validation_dataloader)
+        model.eval()
+
+        total_validation_correct = 0
+        wrong_counter = 0
+        running_loss = 0
+        for validation_images, validation_labels in validationiter:
+
+            validation_images = validation_images.to(device)
+            validation_labels = validation_labels.to(device)
+
+            outputs = model(validation_images)
+            validation_predictions = torch.argmax(outputs, dim=1)
+            
+            v_loss = loss_fn(outputs, validation_labels)
+            running_loss += outputs.shape[0]*v_loss.item()
+
+            correct = torch.sum((validation_predictions == validation_labels).float())
+            total_validation_correct += correct
+            
+        validation_loss = running_loss / n_val
+        validation_accuracy = total_validation_correct*100 / n_val
+        writer.add_scalar("Validation Loss", validation_loss, epoch)
+        writer.add_scalar("Validation Accuracy", validation_accuracy, epoch)
+        print(
+            "Valdation Loss: {:.2f}  Validation Accuracy: {:.2f}".format(validation_loss, validation_accuracy)
+            ,
+        )
+        
+
+        torch.save(model.state_dict(), "{}/{}-{}/checkpoint_{:02d}.pth".format(save_folder, model_name, date_time, epoch))
+
+    torch.save(model.state_dict(), "{}/{}-{}/checkpoint__final.pth".format(save_folder, model_name, date_time))
